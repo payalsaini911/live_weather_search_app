8256e8ebfa621d018b172ca382c7514c1ff0b9c0
payalsaini911 <payalsaini9112002@gmail.com>
2022-07-29 01:35:30 +0530

    weather_search_app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+		'X-RapidAPI-Key': '328105ff3dmshdc815b9e9db9501p129b84jsn8eaf2ab32031',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4aac1b0116d4b0bb0b6a07861273626b"; 
+
